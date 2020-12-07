d307c1694b4ef1fc827710f76b8f5651482365e8
Ivan Hristoslavov <hristoslavov.ivanov@gmail.com>
2020-12-07 23:48:36 +0200

    Add

--- 01. Password Reset/Program.cs
+++ 01. Password Reset/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace _01._Password_Reset
 {
@@ -6,25 +7,55 @@ namespace _01._Password_Reset
     {
         static void Main(string[] args)
         {
-
             string password = Console.ReadLine();
-
             string[] commands = Console.ReadLine().Split(" ");
 
             while (commands[0] != "Done")
             {
+                StringBuilder sb = new StringBuilder();
                 switch (commands[0])
                 {
                     case "TakeOdd":
                         password = TakeOdd(password);
+                        sb.Append(password);
+                        break;
+                    case "Cut":
+                        int firstIndex = int.Parse(commands[1]);
+                        int secondIndex = int.Parse(commands[2]);
+                        password = CutPass(password, firstIndex, secondIndex);
+                        sb.Append(password);
+                        break;
+                    case "Substitute":
+                        if(password.Contains(commands[1]))
+                        {
+                            password = Subtitute(password, commands[1], commands[2]);
+                            sb.Append(password);
+                        }
+                        else
+                        {
+                            sb.Append("Nothing to replace!");
+                        }
                         break;
                 }
-
-
+                Console.WriteLine(sb);
+                commands = Console.ReadLine().Split(" ");
             }
 
+            Console.WriteLine($"Your password is: {password}");
 
+        }
 
+        private static string Subtitute(string password, string forReplace, string replace)
+        {
+            password = password.Replace(forReplace, replace);
+            return password;
+        }
+
+        private static string CutPass(string password, int firstIndex, int secondIndex)
+        {
+
+           password= password.Substring(0,firstIndex)+password.Substring(firstIndex+secondIndex);
+            return password;
         }
 
         private static string TakeOdd(string password)
@@ -38,7 +69,7 @@ namespace _01._Password_Reset
                     newPass += password[i];
                 }
             }
-
+            password = newPass;
             return password;
         }
     }
