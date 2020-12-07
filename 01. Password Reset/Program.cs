76b6cc0efe19f4a4c64870ff44172e600640d887
Ivan Hristoslavov <hristoslavov.ivanov@gmail.com>
2020-12-07 23:18:15 +0200

    Add project files.

---
+++ 01. Password Reset/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _01._Password_Reset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string password = Console.ReadLine();
+
+            string[] commands = Console.ReadLine().Split(" ");
+
+            while (commands[0] != "Done")
+            {
+                switch (commands[0])
+                {
+                    case "TakeOdd":
+                        password = TakeOdd(password);
+                        break;
+                }
+
+
+            }
+
+
+
+        }
+
+        private static string TakeOdd(string password)
+        {
+            string newPass = string.Empty;
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    newPass += password[i];
+                }
+            }
+
+            return password;
+        }
+    }
+}
